2a28ec7a1d846836703a2805340f5fa845d59875
AurelienCaille <aur-lien@hotmail.fr>
2016-10-10 17:57:16 +0200

    add rational.py file
    File of the rational number class

---
+++ rational.py
@@ -0,0 +1,9 @@
+class rational:
+    """
+    Class representative a rational number with a numerator and a denominator
+    """
+    self __init__(self, numerator, denominator):
+
+        self.numerator = numerator
+        self.denominator = denominator
+        
