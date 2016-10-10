3d04eedd423c852c1cbe19f240e41d25645f2c22
AurelienCaille <aur-lien@hotmail.fr>
2016-10-10 18:05:21 +0200

    create multiply method

--- rational.py
+++ rational.py
@@ -2,8 +2,19 @@ class rational:
     """
     Class representative a rational number with a numerator and a denominator
     """
-    self __init__(self, numerator, denominator):
+    def __init__(self, numerator, denominator):
 
         self.numerator = numerator
         self.denominator = denominator
+
+    def multiply (self, rational2):
+        """
+        Multiply the current rational with another and return the result
+        """
+
+        
+    
+        
+
+    
         
