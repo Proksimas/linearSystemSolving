7e43165f026bd31d4174b4cb30cc4f63778777fd
Caille Aurelien <aur-lien@hotmail.fr>
2016-10-24 16:49:09 +0200

    Add gcd function

--- rational.py
+++ rational.py
@@ -1,3 +1,18 @@
+def gcd( integer1, integer2):
+    """
+    Return the value of the gcd between the integer: integer1 and integer2
+    """
+
+    gcd = 1
+
+    for i in reversed(range(2, min(integer1, integer2) + 1)):
+        if integer1 % i == 0 and integer2 % i == 0:
+            gcd = i
+            break
+        
+
+    return gcd
+
 class Rational:
     """
     Class representative a rational number with a numerator and a denominator
@@ -29,7 +44,7 @@ class Rational:
 
         return Rational(num, den)
 
-    def reverse(self)
+    def reverse(self):
         """
         Return the reverse value of the current rational
         """
@@ -43,9 +58,22 @@ class Rational:
 
         return self.multiply(rational2.reverse())
 
-        
+    def simplify(self):
+        """
+        Simplify the current rational
+        """
 
         
+
+if __name__ == "__main__":
+
+    assert(gcd(1, 5) == 1)
+    assert(gcd(5, 10) == 5)
+    assert(gcd(37, 7) == 1)
+
+    a = Rational(2, 3)
+    b = Rational(5, 3)
+    c = Rational(7, 3)
     
         
 
